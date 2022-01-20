50da6f3fd11d80514080e5ee902de8102fd4d293
njeveh <njeveh@gmail.com>
2022-01-20 18:22:39 +0300

    created the initial home page setup

---
+++ wellington/resources/views/home.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset="utf-8">
+        <meta name='viewport' content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' href='css/app.css'>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Rouge+Script&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+        <script>
+            function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "top-navbar") {
+                x.className += " responsive";
+            } else {
+                x.className = "top-navbar";
+            }
+            }
+    </script>
+        <title>Wellington Valuers Limited Kenya</title>
+    </head>
+    <body>
+        <header id='page-header' class='col-12 col-s-12'>
+            <div class='logo-container col-6 col-s-6'>
+                <a href='#'><img src='storage/wellington_logo2.png' id='company-logo'></a>
+                <h2 id='company-name'>WELLINGTON</h2>
+                <h2 id='company-name2'>Valuers</h2>
+            </div>
+            <nav class="top-navbar col-6 col-s-6" id="myTopnav">
+                <a href='#' ><i class='fas fa-home home-icon'> &nbsp;</i><span id='home-link'>Home</span></a>
+                <a href='#' target='blank' >About Us</a>
+                <a href='#' target='blank' >Services</a>
+                <a href='#' target='blank' >Properties</a>
+                <a href='#' target='blank' >Contact Us</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i></a>
+            </nav>
+        </header>
+        <div class='main-image-container col-12 col-s-12' id='main-image-container'>
+            <img src='storage/property6.jpg' class='col-12 col-s-12' id='main-image'>
+        </div>
+        <div id='welcome' class='col-12 col-s-12'>
+            <h2>Welcome</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet facilisis enim.
+                Nunc auctor ante id ex mollis, quis gravida mauris suscipit. Nulla imperdiet pellentesque
+                mauris, eget posuere dui venenatis eu. Suspendisse sed velit non justo vulputate sagittis
+                non eu arcu. Donec felis nunc, euismod mattis est a, finibus eleifend massa. Integer
+                facilisis feugiat lobortis. Mauris vehicula risus id consectetur efficitur...
+            </p>
+        </div>
+        <div class='col-12 col-s-12'>
+            <h2 id='services-title'>Services</h2>
+            <div class='col-4 col-s-4 service-info-container'>
+                <h3 class='service-title'>########## Valuation</h3>
+                <p class='service-info'>
+                    <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet facilisis enim.
+                    Nunc auctor ante id ex mollis, quis gravida mauris suscipit. Nulla imperdiet pellentesque
+                    mauris, eget posuere dui venenatis eu. Suspendisse sed velit non justo vulputate sagittis
+                    non eu arcu. Donec felis nunc, euismod mattis est a, finibus eleifend massa. Integer
+                    facilisis feugiat lobortis. Mauris vehicula risus id consectetur efficitur...
+                    <a href='#'>Reade More &nbsp;<i class='fas fa-caret-right'></i></a>
+                </p>
+            </div>
+            <div class='col-4 c0l-s-4 service-info-container'>
+                <h3 class='service-title'>########## Valuation</h3>
+                <p class='service-info'>
+                    <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet facilisis enim.
+                    Nunc auctor ante id ex mollis, quis gravida mauris suscipit. Nulla imperdiet pellentesque
+                    mauris, eget posuere dui venenatis eu. Suspendisse sed velit non justo vulputate sagittis
+                    non eu arcu. Donec felis nunc, euismod mattis est a, finibus eleifend massa. Integer
+                    facilisis feugiat lobortis. Mauris vehicula risus id consectetur efficitur...
+                    <a href='#'>Reade More &nbsp;<i class='fas fa-caret-right'></i></a>
+                </p>
+            </div>
+            <div class='col-4 col-s-4 service-info-container last'>
+                <h3 class='service-title'>########## Valuation</h3>
+                <p class='service-info'>
+                    <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet facilisis enim.
+                    Nunc auctor ante id ex mollis, quis gravida mauris suscipit. Nulla imperdiet pellentesque
+                    mauris, eget posuere dui venenatis eu. Suspendisse sed velit non justo vulputate sagittis
+                    non eu arcu. Donec felis nunc, euismod mattis est a, finibus eleifend massa. Integer
+                    facilisis feugiat lobortis. Mauris vehicula risus id consectetur efficitur...
+                    <a href='#'>Reade More &nbsp;<i class='fas fa-caret-right'></i></a>
+                </p>
+            </div>
+        </div>
+        <footer class='footer'>
+            <div class='col-4 col-s-4 footerdiv'>
+                <h3>About Us</h3>
+                <p>
+                    <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet facilisis enim.
+                    Nunc auctor ante id ex mollis, quis gravida mauris suscipit. Nulla imperdiet pellentesque
+                    mauris, eget posuere dui venenatis eu. Suspendisse sed velit non justo vulputate sagittis
+                    non eu arcu. Donec felis nunc, euismod mattis est a, finibus eleifend massa. Integer
+                    facilisis feugiat lobortis. Mauris vehicula risus id consectetur efficitur...
+                    <a href='#'>Reade More &nbsp;<i class='fas fa-caret-right'></i></a>
+                </p>
+
+
+            </div>
+            <div class='col-4 col-s-4 footerdiv'>
+                <h3>Get in touch</h3>
+                <p>
+                    <br>266 Muindi-Mbingu Street, xxxxx Plaza, 4th floor<br>
+                    Nairobi, xx, xxxx<br><br>
+                    +2547xxxxxxxx<br><br>
+                    info@wellingtonvaluers.com
+                </p>
+            </div>
+            <div class='col-4 col-s-4 footerdiv'>
+                <h3>Useful Links</h3>
+                <ul class="footer-links">
+                    <li>
+                        <br><a href='#' target='blank' >About Us</a><br><br>
+                    </li>
+                    <li>
+                        <a href='#' target='blank' >Services</a><br><br>
+                    </li>
+                    <li>
+                        <a href='#' target='blank' >Properties</a><br><br>
+                    </li>
+                    <li>
+                        <a href='#' target='blank' >Contact Us</a><br>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
